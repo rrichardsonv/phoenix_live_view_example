292a740c2208f8feee4c2e5bbe6020f53c3b0dcc
Rich Richardson <rrichardsonv+github@gmail.com>
2019-08-30 09:20:24 -0500

    Seems to work

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,37 @@ import {LiveSocket, debug} from "phoenix_live_view"
 let logger =  function(kind, msg, data) {
   // console.log(`${kind}: ${msg}`, data)
 }
-let liveSocket = new LiveSocket("/live", {logger: logger})
+
+let serializeForm = (form) => {
+  let formData = new FormData(form)
+  let params = new URLSearchParams()
+  for(let [key, val] of formData.entries()){ params.append(key, val) }
+
+  return params.toString()
+}
+
+let Params = {
+  data: {},
+  set(namespace, key, val){
+    if(!this.data[namespace]){ this.data[namespace] = {}}
+    this.data[namespace][key] = val
+  },
+  get(namespace){ return this.data[namespace] || {} }
+}
+
+let Hooks = {}
+Hooks.SavedForm = {
+  mounted(){
+    this.el.addEventListener("input", e => {
+      Params.set(this.viewName, "stashed_form", serializeForm(this.el))
+    })
+  }
+}
+
+let liveSocket = new LiveSocket("/live", {hooks: Hooks, params: (view) => {
+  console.log("Getting here", view);
+  return Params.get(view)
+},
+})
 liveSocket.connect()
 window.liveSocket = liveSocket
